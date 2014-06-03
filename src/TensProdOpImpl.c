5d0672c7459e5d35155788b184ae1af38aeedabd
Dominic Meiser <dmeiser@txcorp.com>
2014-06-02 18:21:33 -0600

    Reorganizing files.

--- src/TensProdOpImpl.c
+++ src/TensProdOpImpl.c
@@ -1,2 +1,1 @@
 #include "TensProdOpImpl.h"
-
--- src/test_TensProdOp.c
+++ src/test_TensProdOp.c
@@ -3,15 +3,14 @@
 
 int test_embed()
 {
-  int errs = 0;
-  TOp op = embed(0, 0, 0);
-  return errs;
+	int errs = 0;
+	TOp op = embed(0, 0, 0);
+	return errs;
 }
 
 int main()
 {
 	int errs = 0;
-  errs += test_embed();
+	errs += test_embed();
 	return errs;
 }
-
