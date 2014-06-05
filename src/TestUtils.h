67a80cb89f87b8c806cab74486e2c667e522ad2f
Dominic Meiser <dmeiser@txcorp.com>
2014-06-05 13:46:09 -0600

    Add test macro to check for closeness (floating point).

--- src/TestUtils.h
+++ src/TestUtils.h
@@ -1,14 +1,21 @@
 #ifndef TEST_UTILS_H
 #define TEST_UTILS_H
 
+double fabs(double);
+
 #define CHK_EQUAL(lhs, rhs, err)                                               \
 	do {                                                                   \
-		if (!((lhs) == (rhs))) ++err;                                  \
+		if ((lhs) != (rhs)) ++err;                                     \
+	} while (0)
+
+#define CHK_CLOSE(lhs, rhs, eps, err)                                          \
+	do {                                                                   \
+		if (fabs(lhs - rhs) > eps) ++err;                              \
 	} while (0)
 
 #define CHK_NOT_EQUAL(lhs, rhs, err)                                           \
 	do {                                                                   \
-		if (!((lhs) != (rhs))) ++err;                                  \
+		if ((lhs) == (rhs)) ++err;                                     \
 	} while (0)
 
 #endif
