343f10042da27766ac65ec1fa49668e9c6798751
Dominic Meiser <dmeiser@txcorp.com>
2014-08-02 23:28:55 -0600

    Implement mapping/unmapping for MboVecs.

--- include/MboVec.h
+++ include/MboVec.h
@@ -12,10 +12,37 @@ typedef struct MboVec *MboVec;
 
 
 /** @brief Create MboVec of dimension dim */
-void mboVecCreate(int dim, MboVec *v);
+void mboVecCreate(long dim, MboVec *v);
 
 /** @brief Release all resources of a MboVec */
 void mboVecDestroy(MboVec *v);
+
+/** @brief Obtain a read-write view of the vector data
+ *
+ * * The view has to be released using mboVecReleaseView before the vector can
+ * be used again.  Getting a modifiable array may incur additional
+ * synchronization overheads.  If the array contents doesn't need to be modified
+ * better performance may be obtained with mboVecGetViewR.
+ *
+ * @see mboVecReleaseView, mboVecGetViewR
+ * */
+int mboVecGetViewRW(MboVec v, struct MboAmplitude **array);
+
+/** @brief Obtain a read-only view of vector data
+ *
+ * The view has to be released using mboVecReleaseView before the vector can be
+ * used again.  Any modifications to the array lead to undefined behaviour.  To
+ * obtain a modifiable array use mboVecGetViewRW.
+ *
+ * @see mboVecReleaseView, mboVecGetViewRW
+ * */
+int mboVecGetViewR(MboVec v, struct MboAmplitude **array);
+
+/** @brief Release a view of the vector data. */
+int mboVecReleaseView(MboVec v, struct MboAmplitude **array);
+
+/** @brief Check integrity of MboVec */
+int mboVecCheck();
 
 /** @brief Run MboVec tests */
 int mboVecTest();
--- src/MboVec.c
+++ src/MboVec.c
@@ -1,9 +1,175 @@
+#include <stdlib.h>
 #include <MboVec.h>
+#include <MboErrors.h>
+#include <MboAmplitude.h>
+
+enum MBO_VEC_MAPPING_STATUS {
+	MBO_VEC_MAPPING_STATUS_UNMAPPED = 0,
+	MBO_VEC_MAPPING_STATUS_MAPPED_R,
+	MBO_VEC_MAPPING_STATUS_MAPPED_RW,
+};
+
+struct MboVec {
+	long dim;
+	enum MBO_VEC_MAPPING_STATUS mapped;
+	struct MboAmplitude *array;
+};
+
+void mboVecCreate(long dim, MboVec *v)
+{
+	*v = malloc(sizeof(**v));
+	(*v)->dim = dim;
+	(*v)->mapped = MBO_VEC_MAPPING_STATUS_UNMAPPED;
+	(*v)->array = malloc(dim * sizeof(*(*v)->array));
+}
+
+void mboVecDestroy(MboVec *v)
+{
+	free((*v)->array);
+	free(*v);
+	*v = 0;
+}
+
+int mboVecCheck(MboVec v)
+{
+	int errs = 0;
+	return errs;
+}
+
+static int mboVecGetView(MboVec v, struct MboAmplitude **array,
+			 enum MBO_VEC_MAPPING_STATUS mapping)
+{
+	if (v->mapped) {
+		*array = 0;
+		return MBO_VEC_IN_USE;
+	}
+	*array = v->array;
+	v->mapped = mapping;
+	return MBO_SUCCESS;
+}
+
+int mboVecGetViewRW(MboVec v, struct MboAmplitude **array)
+{
+	return mboVecGetView(v, array, MBO_VEC_MAPPING_STATUS_MAPPED_RW);
+}
+
+int mboVecGetViewR(MboVec v, struct MboAmplitude **array)
+{
+	return mboVecGetView(v, array, MBO_VEC_MAPPING_STATUS_MAPPED_R);
+}
+
+int mboVecReleaseView(MboVec v, struct MboAmplitude **array)
+{
+	v->mapped = MBO_VEC_MAPPING_STATUS_UNMAPPED;
+	*array = 0;
+	return MBO_SUCCESS;
+}
 
 #include "TestUtils.h"
+#define EPS 1.0e-12
+
+static int testMboVecCreate()
+{
+	int errs = 0;
+	MboVec v;
+
+	mboVecCreate(5, &v);
+	CHK_TRUE(v != 0, errs);
+	mboVecDestroy(&v);
+
+	return errs;
+}
+
+static int testMboVecDestroy()
+{
+	int errs = 0;
+	MboVec v;
+
+	mboVecCreate(5, &v);
+	mboVecDestroy(&v);
+	CHK_EQUAL(v, 0, errs);
+
+	return errs;
+}
+
+static int testMboVecCheck()
+{
+	int errs = 0;
+	MboVec v;
+
+	mboVecCreate(5, &v);
+	CHK_EQUAL(mboVecCheck(v), 0, errs);
+	mboVecDestroy(&v);
+
+	return errs;
+}
+
+static int testMboVecGetViewRW()
+{
+	int errs = 0, err, i, n;
+	MboVec v;
+	struct MboAmplitude *arr;
+
+	n = 5;
+	mboVecCreate(n, &v);
+	err = mboVecGetViewRW(v, &arr);
+	CHK_EQUAL(err, MBO_SUCCESS, errs);
+	CHK_TRUE(arr != 0, errs);
+	for (i = 0; i < n; ++i) {
+		arr[i].re = i;
+		arr[i].im = -2.0 * i;
+	}
+	err = mboVecReleaseView(v, &arr);
+	CHK_EQUAL(err, MBO_SUCCESS, errs);
+	CHK_TRUE(arr == 0, errs);
+	mboVecDestroy(&v);
+
+	return errs;
+}
+
+static int testMboVecGetViewR()
+{
+	int errs = 0, n = 5, err, i;
+	MboVec v;
+	struct MboAmplitude *arr;
+
+	mboVecCreate(n, &v);
+	err = mboVecGetViewR(v, &arr);
+	CHK_EQUAL(err, MBO_SUCCESS, errs);
+	CHK_TRUE(arr != 0, errs);
+	err = mboVecGetViewR(v, &arr);
+	CHK_EQUAL(err, MBO_VEC_IN_USE, errs);
+	CHK_TRUE(arr == 0, errs);
+	err = mboVecReleaseView(v, &arr);
+	CHK_EQUAL(err, MBO_SUCCESS, errs);
+
+	err = mboVecGetViewRW(v, &arr);
+	for (i = 0; i < n; ++i) {
+		arr[i].re = i;
+		arr[i].im = -2.0 * i;
+	}
+	err = mboVecReleaseView(v, &arr);
+	err = mboVecGetViewR(v, &arr);
+	CHK_EQUAL(err, MBO_SUCCESS, errs);
+	for (i = 0; i < n; ++i) {
+		CHK_CLOSE(arr[i].re, i, EPS, errs);
+		CHK_CLOSE(arr[i].im, -2.0 * i, EPS, errs);
+	}
+	mboVecReleaseView(v, &arr);
+
+	mboVecDestroy(&v);
+
+	return errs;
+}
 
 int mboVecTest()
 {
-  return 0;
+	int errs = 0;
+	errs += testMboVecCreate();
+	errs += testMboVecDestroy();
+	errs += testMboVecCheck();
+	errs += testMboVecGetViewRW();
+	errs += testMboVecGetViewR();
+	return errs;
 }
 
