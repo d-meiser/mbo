f3e2df7426c092870006b0993346a254ac834e1a
Dominic Meiser <dmeiser@txcorp.com>
2014-08-10 18:38:46 -0600

    Complete TavisCummings example.

--- examples/TavisCummings.c
+++ examples/TavisCummings.c
@@ -1,34 +1,22 @@
 #include <Mbo.h>
 #include <stdio.h>
 
-double omega(int i);
+static const int nAtoms = 10;
+static const int nPhotons = 30;
+static const double omegaC = 2.345e0;
+
+static double omega(int i);
 
 int main()
 {
-	MboProdSpace hSingleAtom;
-	MboProdSpace hAtoms;
-	MboProdSpace hField;
-	MboProdSpace hTot;
-	MboElemOp sm;
-	MboElemOp sp;
-	MboElemOp sz;
-	const int nAtoms = 20;
-	const int nPhotons = 30;
-	MboTensorOp inhomogeneousJz;
-	MboTensorOp jMinus;
+	MboProdSpace hSingleAtom, hAtoms, hField, hTot;
+	MboElemOp sm, sp, sz, a, ad, numberOp;
+	MboTensorOp inhomogeneousJz, jPlus, jMinus, idField, idAtoms, A, Ad, N,
+	    H;
 	struct MboAmplitude tmp;
+	MboVec x, y;
 
-	mboElemOpCreate(&sm);
-	tmp.re = 1.0;
-	tmp.im = 0.0;
-	mboElemOpAddTo(0, 1, &tmp, &sm);
-	mboElemOpCreate(&sp);
-	mboElemOpAddTo(1, 0, &tmp, &sp);
-	mboElemOpCreate(&sz);
-	mboElemOpAddTo(1, 1, &tmp, &sz);
-	tmp.re = -1.0;
-	mboElemOpAddTo(0, 0, &tmp, &sz);
-
+	/* build various Hilbert spaces */
 	hSingleAtom = mboProdSpaceCreate(2);
 	hAtoms = mboProdSpaceCreate(0);
 	for (int i = 0; i < nAtoms; ++i) {
@@ -39,23 +27,80 @@ int main()
 	mboProdSpaceMul(hAtoms, &hTot);
 	mboProdSpaceMul(hField, &hTot);
 
+	/* build atomic operators */
+	sm = mboSigmaMinus();
+	sp = mboSigmaPlus();
+	sz = mboSigmaZ();
 	mboTensorOpNull(hAtoms, &inhomogeneousJz);
 	for (int i = 0; i < nAtoms; ++i) {
 		tmp.re = omega(i);
+		tmp.im = 0;
 		mboTensorOpAddScaledTo(&tmp, sz, i, inhomogeneousJz);
 	}
+	mboTensorOpNull(hAtoms, &jMinus);
+	for (int i = 0; i < nAtoms; ++i) {
+		mboTensorOpAddTo(sm, i, jMinus);
+	}
+	mboTensorOpNull(hAtoms, &jPlus);
+	for (int i = 0; i < nAtoms; ++i) {
+		mboTensorOpAddTo(sp, i, jPlus);
+	}
+	mboTensorOpIdentity(hAtoms, &idAtoms);
 
-	mboTensorOpNull(hTot, &jMinus);
+	/* build field operators */
+	a = mboAnnihilationOp(nPhotons + 1);
+	ad = mboCreationOp(nPhotons + 1);
+	numberOp = mboNumOp(nPhotons + 1);
+	mboTensorOpNull(hField, &A);
+	mboTensorOpAddTo(a, 0, A);
+	mboTensorOpNull(hField, &Ad);
+	mboTensorOpAddTo(ad, 0, Ad);
+	mboTensorOpNull(hField, &N);
+	/* The frequency of the cavity */
+	tmp.re = omegaC;
+	tmp.im = 0;
+	mboTensorOpAddScaledTo(&tmp, numberOp, 0, N);
+	mboTensorOpIdentity(hField, &idField);
 
+	/* Assemble Hamiltonian */
+	mboTensorOpNull(hTot, &H);
+	mboTensorOpKron(idField, inhomogeneousJz, &H);
+	mboTensorOpKron(N, idAtoms, &H);
+	mboTensorOpKron(A, jPlus, &H);
+	mboTensorOpKron(Ad, jMinus, &H);
+
+	printf("Dimension of total Hilbert Space: %ld\n",
+	       mboProdSpaceDim(hTot));
+
+	mboVecCreate(mboProdSpaceDim(hTot), &x);
+	mboVecCreate(mboProdSpaceDim(hTot), &y);
+
+	tmp.re = 1.0;
+	tmp.im = 0.0;
+	mboTensorOpMatVec(&tmp, H, x, &tmp, y);
+
+	/* Release all resources */
 	mboElemOpDestroy(&sm);
 	mboElemOpDestroy(&sp);
 	mboElemOpDestroy(&sz);
+	mboElemOpDestroy(&a);
+	mboElemOpDestroy(&ad);
+	mboElemOpDestroy(&numberOp);
 	mboProdSpaceDestroy(&hSingleAtom);
 	mboProdSpaceDestroy(&hAtoms);
 	mboProdSpaceDestroy(&hField);
 	mboProdSpaceDestroy(&hTot);
 	mboTensorOpDestroy(&inhomogeneousJz);
 	mboTensorOpDestroy(&jMinus);
+	mboTensorOpDestroy(&jPlus);
+	mboTensorOpDestroy(&idAtoms);
+	mboTensorOpDestroy(&A);
+	mboTensorOpDestroy(&Ad);
+	mboTensorOpDestroy(&N);
+	mboTensorOpDestroy(&idField);
+	mboTensorOpDestroy(&H);
+	mboVecDestroy(&x);
+	mboVecDestroy(&y);
 
 	return 0;
 }
