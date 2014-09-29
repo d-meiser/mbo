cf2788bd212368c92b16f12fdaafe9ad62e0709a
Dominic Meiser <dmeiser@txcorp.com>
2014-09-28 21:14:45 -0600

    Finish OpenMP example.
    This currently segfaults due to a bug in applyEmbeddings.

--- examples/OpenMP.c
+++ examples/OpenMP.c
@@ -22,60 +22,117 @@ with mbo.  If not, see <http://www.gnu.org/licenses/>.
  * */
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <math.h>
 #include "Mbo.h"
 
-static const int numSpins = 12;
+static const int numSpins = 13;
 static const int numIters = 10;
 
-int main()
+static MboProdSpace buildSpace(int n)
 {
-	MboElemOp sp, sm;
-	MboTensorOp Jx;
 	MboProdSpace h1, hTot;
-  struct MboAmplitude one, zero, pointFive, *x, *y;
-  int i;
-  MboGlobInd n;
+	int i;
 
-	/* Build Hilbert space */
 	h1 = mboProdSpaceCreate(2);
 	hTot = mboProdSpaceCreate(0);
 	for (i = 0; i < numSpins; ++i) {
 		mboProdSpaceMul(h1, &hTot);
 	}
 	mboProdSpaceDestroy(&h1);
-	
-	/* Build Jx */
+	return hTot;
+}
+
+static MboTensorOp buildJx(MboProdSpace h)
+{
+	MboElemOp sp, sm;
+	MboTensorOp Jx;
+	struct MboAmplitude pointFive;
+	int i;
+
 	sp = mboSigmaPlus();
 	sm = mboSigmaMinus();
 	pointFive.re = 0.5;
 	pointFive.im = 0.0;
-	mboTensorOpNull(hTot, &Jx);
+	mboTensorOpNull(h, &Jx);
 	for (i = 0; i < numSpins; ++i) {
 		mboTensorOpAddScaledTo(&pointFive, sm, i, Jx);
 		mboTensorOpAddScaledTo(&pointFive, sp, i, Jx);
 	}
 	mboElemOpDestroy(&sp);
 	mboElemOpDestroy(&sm);
+	return Jx;
+}
 
-	/* Compute matrix */
+int main()
+{
+	MboTensorOp Jx;
+	MboProdSpace hTot;
+	struct MboAmplitude one, zero, *x, *y, *yomp;
+	int i;
+	MboGlobInd n, chunk, chunkSize, numChunks;
+	clock_t tstart, tend;
+	double deltat, difference;
+
+	hTot = buildSpace(numSpins);
+	Jx = buildJx(hTot);
+
 	one.re = 1.0;
 	one.im = 0.0;
 	zero.re = 0.0;
 	zero.im = 0.0;
+	x = malloc(mboProdSpaceDim(hTot) * sizeof(*x));
+	y = malloc(mboProdSpaceDim(hTot) * sizeof(*x));
+	yomp = malloc(mboProdSpaceDim(hTot) * sizeof(*x));
+	for (n = 0; n < mboProdSpaceDim(hTot); ++n) {
+		x[n].re = (double)rand() / RAND_MAX;
+		x[n].im = (double)rand() / RAND_MAX;
+	}
 
-  x = malloc(mboProdSpaceDim(hTot) * sizeof(*x));
-  y = malloc(mboProdSpaceDim(hTot) * sizeof(*x));
-  for (n = 0; n < mboProdSpaceDim(hTot); ++n) {
-    x[n].re = (double)rand() / RAND_MAX;
-    x[n].im = (double)rand() / RAND_MAX;
-  }
-  for (i = 0; i < numIters; ++i) {
-    mboTensorOpMatVec(one, Jx, x, zero, y, 0, mboProdSpaceDim(hTot));
-  }
 
-	/* Deallocate remaining resources */
-  free(x);
-  free(y);
+	tstart = clock();
+	for (i = 0; i < numIters; ++i) {
+		mboTensorOpMatVec(one, Jx, x, zero, y, 0,
+				  mboProdSpaceDim(hTot));
+	}
+	tend = clock();
+	deltat = (double)(tend - tstart) / (double)CLOCKS_PER_SEC;
+	printf("Serial: %2.3lf s\n", deltat);
+
+	tstart = clock();
+	chunkSize = 1024;
+	printf("Chunk Size: %lld\n", chunkSize);
+	numChunks = mboProdSpaceDim(hTot) / chunkSize;
+	printf("Number of Chunks: %lld\n", numChunks);
+	if (numChunks * chunkSize < mboProdSpaceDim(hTot)) ++numChunks;
+	for (i = 0; i < numIters; ++i) {
+		for (chunk = 0; chunk < numChunks; ++chunk) {
+			mboTensorOpMatVec(one, Jx, x + chunk * chunkSize, zero,
+					  yomp + chunk * chunkSize,
+					  chunk * chunkSize,
+					  (chunk + 1) * chunkSize);
+		}
+	}
+	tend = clock();
+	deltat = (double)(tend - tstart) / (double)CLOCKS_PER_SEC;
+	printf("Parallel: %2.3lf s\n", deltat);
+
+	difference = 0;
+	for (n = 0; n < mboProdSpaceDim(hTot); ++n) {
+		difference += (yomp[n].re - y[n].re) * (yomp[n].re - y[n].re) +
+			      (yomp[n].im - y[n].im) * (yomp[n].im - y[n].im);
+	}
+	difference = sqrt(difference);
+	printf("Error: %lf\n", difference);
+	if (difference < 1.0e-12) {
+		return 0;
+	} else {
+		return 1;
+	}
+
+	free(x);
+	free(y);
+	free(yomp);
 	mboTensorOpDestroy(&Jx);
 	mboProdSpaceDestroy(&hTot);
 }
