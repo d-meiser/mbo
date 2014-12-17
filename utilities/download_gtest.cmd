a780925c40f7c3b3ea50a1a9f48e49ebc3c0984f
Dominic Meiser <dmeiser@txcorp.com>
2014-12-17 08:46:16 -0700

    Trying to fix the windows download script.

--- utilities/download_gtest.cmd
+++ utilities/download_gtest.cmd
@@ -1,3 +1,4 @@
-(new-object System.Net.WebClient).DownloadFile("http://googletest.googlecode.com/files/gtest-1.7.0.zip","gtest-1.7.0.zip")
+$client = new-object System.Net.WebClient
+$client.DownloadFile("http://googletest.googlecode.com/files/gtest-1.7.0.zip","gtest-1.7.0.zip")
 unzip gtest-1.7.0.zip
 rm gtest-1.7.0.zip
